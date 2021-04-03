f78791819ebfcb3a39bf61e5538af5bfba041ca3
Aleksander Tkachev <bobor9752@gmail.com>
2021-04-03 14:18:33 +0300

    practice part 3

--- js/script.js
+++ js/script.js
@@ -1,19 +1,69 @@
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', []);
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', []);
+
+    while( numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', []);
+    }
+}
+
+start();
+
 let personalMoveDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     private: false
-};
+};      
 
-let OfFilms = prompt('Один из последних посмотренных фильмов', []);
-let appraisal = prompt('На сколько оцените его?', []);
-let OfFilms1 = prompt('Один из последних посмотренных фильмов', []);
-let appraisal1 = prompt('На сколько оцените его?', []);
+let num = 0;
+function rememberMyFilms(){
+    for(let i = 0; i < 2; i++){
+        let OfFilms = prompt('Один из последних посмотренных фильмов', []);
+        let appraisal = prompt('На сколько оцените его?', []);
+        if(OfFilms != '' && appraisal != '' && OfFilms != null && appraisal != null && OfFilms.length < 50){
+            personalMoveDB.movies[OfFilms] = appraisal;
+            console.log('test');
+        }else {
+            console.log('error');
+            i--;
+        }
+    }
+}
 
-personalMoveDB.movies[OfFilms] = appraisal;
-personalMoveDB.movies[OfFilms1] = appraisal1;
+rememberMyFilms();
 
-console.log(personalMoveDB);
+function detectPersonalLevel(){
+    if(personalMoveDB.count < 10) {
+        alert('Посмотрено довольно мало фильмов');
+    }else if (personalMoveDB.count >= 10 && personalMoveDB.count < 30) {
+        alert('Вы класический зритель');
+    }else if (personalMoveDB.count >= 30) {
+        alert('Вы киноман');
+    }else {
+        alert('Произошла ошибка');
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if(hidden == false) {
+        console.log(personalMoveDB);
+    }
+}
+
+showMyDB(personalMoveDB.private);
+
+function writeYourGeneres() {
+    for(let i =1; i <= 3; i++){
+        personalMoveDB.genres[i-1] = prompt('Ваш любимый жанр под номером ' + i);
+    }
+}
+
+writeYourGeneres();
+
+// console.log(personalMoveDB);
